43a06185f7fcad5e77fe84500745ef8ba2e844e1
Andre Skeene <halodre24@gmail.com>
2020-02-19 23:10:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,9 @@
 def prime?(num)
   new_array = (-1..(num + 1)).to_a
   
-  if new_array <= 1 
-    return false
-end
+if new_array.include?(2)
+    return true
+  elsif new_array <= -1
+    false
+  end
 end
