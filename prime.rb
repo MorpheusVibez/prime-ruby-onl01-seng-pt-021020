18e43e6b4c48ceac3df40e5d60fd08ffb8669d56
Andre Skeene <halodre24@gmail.com>
2020-02-19 22:41:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,3 @@
 def prime?(num)
-  new_array = (2..(num - 1)).to_a
-
-  if new_array.any? {|divisor| num % divisor == 0} || integer == 0 || integer == -1 ? false : true
-    false	
-end
-
+  new_array = (-1..(num + 1)).to_a
 end
